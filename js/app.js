85bf46431c63b286eb510a62a4da9c560e749d57
Dkaf <Daniel.j.kafer@gmail.com>
2016-08-12 18:24:26 -0500

    random number and if statements
    added random number generator and if statements to check distance

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +12,40 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+		var guess = parseInt($('form'));
+
+		console.log(guess);
+		function randomNum() {
+			var answer =	Math.floor(Math.random() * (100 - 1) + 1);
+			return answer;
+		}
+
+		var secretNum = randomNum();
+
+		console.log(secretNum);
+
+		if (guess >= secretNum + 50 || guess <= secretNum - 50) {
+			$('#feedback h2').text('Ice Cold');
+		}
+
+		else if (guess >= secretNum + 30 && guess < secretNum + 50 || guess <= secretNum - 30 && guess > secretNum - 50 ) {
+			$('#feedback h2').text('Cold');
+		}
+
+		else if (guess >= secretNum + 20 && guess < secretNum + 30 || guess <= secretNum - 20 && guess > secretNum - 30) {
+			$('#feedback h2').text('Warm');
+		}
+
+		else if (guess >= secretNum + 10 && guess < secretNum + 20 || guess <= secretNum - 10 && guess > secretNum - 20) {
+			$('#feedback h2').text('Hot');
+		}
+
+		else if (guess >= secretNum + 1 && guess < secretNum + 10 || guess <= secretNum - 1 && guess > secretNum -10) {
+			$('#feedback h2').text('Very Hot!');
+		}
+
+		else if (guess == secretNum) {
+			$('#feedback h2').text('You guessed the Number!');
+		}
+
 });
-
-
