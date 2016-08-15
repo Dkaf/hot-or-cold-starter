1d3ac07d5270540578b3f50e2911f4f8130d129f
Dkaf <Daniel.j.kafer@gmail.com>
2016-08-15 14:27:10 -0500

    Finised hot cold guessing game

--- js/app.js
+++ js/app.js
@@ -12,40 +12,85 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-		var guess = parseInt($('form'));
 
-		console.log(guess);
+
+		/*--- Generating random number ---*/
 		function randomNum() {
 			var answer =	Math.floor(Math.random() * (100 - 1) + 1);
 			return answer;
 		}
 
 		var secretNum = randomNum();
-
+		var guessCount = 0;
+		var previousGuess = 0;
 		console.log(secretNum);
 
-		if (guess >= secretNum + 50 || guess <= secretNum - 50) {
-			$('#feedback h2').text('Ice Cold');
+		$('#guessButton').on('click', function (e){
+			e.preventDefault();
+			var guess = parseInt($('#userGuess').val());
+			$('#userGuess').val('')
+			if (validate(guess)) {
+				$('#guessList').append('<li>' + guess + '</li>');
+				console.log(guess);
+				game(guess);
+				guessCount += 1;
+				$('#count').html(guessCount.toString())
+			}
+		})
+
+		$('.new').on('click', function () {
+			secretNum = randomNum();
+			$('#feedback').html('Make your Guess!')
+			$('#guessList li').remove();
+			$('#userGuess').val('')
+			$('#count').html('0');
+			guessCount = 0;
+		})
+
+		function validate(guess) {
+			if (guess > 100 || isNaN(guess)) {
+			$('#feedback').html('Please pick a number between 1 and 100');
+			return false;
+			}
+			return true;
 		}
 
-		else if (guess >= secretNum + 30 && guess < secretNum + 50 || guess <= secretNum - 30 && guess > secretNum - 50 ) {
-			$('#feedback h2').text('Cold');
-		}
+		/*--- Checking guess distance from answer ---*/
+		function game(guess) {
 
-		else if (guess >= secretNum + 20 && guess < secretNum + 30 || guess <= secretNum - 20 && guess > secretNum - 30) {
-			$('#feedback h2').text('Warm');
-		}
+			if (guess == secretNum) {
+					$('#feedback').html('You guessed the Number!');
+				}
+			else if (previousGuess == 0) {
 
-		else if (guess >= secretNum + 10 && guess < secretNum + 20 || guess <= secretNum - 10 && guess > secretNum - 20) {
-			$('#feedback h2').text('Hot');
-		}
+			  if (guess >= secretNum + 1 && guess < secretNum + 10 || guess <= secretNum - 1 && guess > secretNum -10) {
+					$('#feedback').html('Very Hot!');
+				}
 
-		else if (guess >= secretNum + 1 && guess < secretNum + 10 || guess <= secretNum - 1 && guess > secretNum -10) {
-			$('#feedback h2').text('Very Hot!');
-		}
+				else if (guess >= secretNum + 10 && guess < secretNum + 20 || guess <= secretNum - 10 && guess > secretNum - 20) {
+					$('#feedback').html('Hot');
+				}
 
-		else if (guess == secretNum) {
-			$('#feedback h2').text('You guessed the Number!');
-		}
+				else if (guess >= secretNum + 20 && guess < secretNum + 30 || guess <= secretNum - 20 && guess > secretNum - 30) {
+					$('#feedback').html('Warm');
+				}
 
+				else if (guess >= secretNum + 30 && guess < secretNum + 50 || guess <= secretNum - 30 && guess > secretNum - 50 ) {
+					$('#feedback').html('Cold');
+				}
+
+				else if (guess >= secretNum + 50 && guess < 101 || guess <= secretNum - 50 && guess < 101) {
+					$('#feedback').html('Ice Cold');
+				}
+			}
+			else {
+				if (Math.abs(secretNum - guess) > Math.abs(secretNum - previousGuess)) {
+					$('#feedback').html('Colder');
+				}
+				else {
+					$('#feedback').html('Warmer');
+				}
+			}
+			previousGuess = guess;
+	}
 });
